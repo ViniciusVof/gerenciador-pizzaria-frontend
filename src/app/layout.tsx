4f6f81ab161e46fa7e4f0ef1dedae31ec7d10d0a
Vinicius Freitas <viniciusfreitas@MacBook-Pro-de-MacInBox.local>
2023-11-16 10:27:58 -0300

    iniciando estrutura e componente de input

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "Gerenciador",
+};
+import "@/styles/globals.scss";
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>{children}</body>
+    </html>
+  );
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,22 @@
+export const metadata = {
+  title: "Gerenciador - Login",
+};
+
+import Image from "next/image";
+import logoImg from "../../public/logo.svg";
+import styles from "@/styles/home.module.scss";
+import { Input } from "@/components/ui/Input";
+
+export default function Home() {
+  return (
+    <div className={styles.containerCenter}>
+      <Image src={logoImg} alt="Logo Gerenciador" />
+      <div className={styles.login}>
+        <form>
+          <Input placeholder="Digite seu e-mail" type="text" />
+          <Input placeholder="Digite sua senha" type="password" />
+        </form>
+      </div>
+    </div>
+  );
+}
