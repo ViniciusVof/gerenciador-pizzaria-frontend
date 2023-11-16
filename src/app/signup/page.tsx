86ed2bd30529d91a9e9e3a21a38dbcd87ceddb8c
Vinicius Freitas <viniciusfreitas@MacBook-Pro-de-MacInBox.local>
2023-11-16 11:01:33 -0300

    Interface cadastro

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,29 +1,31 @@
 export const metadata = {
-  title: "Gerenciador - Login",
+  title: "Gerenciador - Cadastro",
 };
 
 import Image from "next/image";
-import logoImg from "../../public/logo.svg";
+import logoImg from "../../../public/logo.svg";
 import styles from "@/styles/home.module.scss";
 import { Input } from "@/components/ui/Input";
 import { Button } from "@/components/ui/Button";
 import Link from "next/link";
 
-export default function Home() {
+export default function SignUp() {
   return (
     <div className={styles.containerCenter}>
       <Image src={logoImg} alt="Logo Gerenciador" />
       <div className={styles.login}>
+        <h1>Criando sua conta</h1>
         <form>
+          <Input placeholder="Digite seu nome" type="text" />
           <Input placeholder="Digite seu e-mail" type="text" />
           <Input placeholder="Digite sua senha" type="password" />
           <Button type="submit" loading={false}>
-            Acessar
+            Cadastrar
           </Button>
         </form>
 
         <Link href="/signup" className={styles.text}>
-          Não possui uma conta? Cadastre-se
+          Já possui uma conta? Faça o login
         </Link>
       </div>
     </div>
