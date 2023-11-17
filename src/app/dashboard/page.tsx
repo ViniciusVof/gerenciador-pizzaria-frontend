f0598c4d482b98849dc37d09faf8e4c8703444aa
Vinicius Freitas <viniciusfreitas@MacBook-Pro-de-MacInBox.local>
2023-11-17 14:34:33 -0300

    Listando mesas abertas

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,51 @@
-import { Header } from "@/components/Header";
+"use client";
+import { FiRefreshCcw } from "react-icons/fi";
+import styles from "./styles.module.scss";
+import { useContext, useEffect, useState } from "react";
+import { LoadingContext } from "@/contexts/LoadingContext";
+import { api } from "@/services/apiClient";
+import { toast } from "react-toastify";
 
 export default function Dashboard() {
+  const { setLoadingPage } = useContext(LoadingContext);
+  const [orders, setOrders] = useState([]);
+
+  function getOrders() {
+    setLoadingPage(true);
+    api
+      .get("/orders")
+      .then((response) => {
+        setOrders(response.data);
+      })
+      .catch(() => toast.error("Erro ao listar pedidos"))
+      .finally(() => setLoadingPage(false));
+  }
+  useEffect(() => {
+    getOrders();
+  }, []);
   return (
     <>
       <title>Painel Gerenciador</title>
 
-      <div>
-        <h1>Painel</h1>
-      </div>
+      <main className={styles.container}>
+        <div className={styles.containerHeader}>
+          <h1>Últimos pedidos</h1>
+          <button>
+            <FiRefreshCcw size={25} color="#3fffa3" />
+          </button>
+        </div>
+
+        <article className={styles.listOrders}>
+          {orders.map(({ id, table }) => (
+            <section key={id} className={styles.orderItem}>
+              <button>
+                <div className={styles.tag}></div>
+                <span>Mesa {table}</span>
+              </button>
+            </section>
+          ))}
+        </article>
+      </main>
     </>
   );
 }
